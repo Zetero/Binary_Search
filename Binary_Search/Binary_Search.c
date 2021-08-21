0ad9ee2c5f515e603c03dbb246de493afd8e1952
Zetero <opelogiena@gmail.com>
2021-08-21 19:30:30 +0300

    Update Binary_Search.c

--- Binary_Search/Binary_Search.c
+++ Binary_Search/Binary_Search.c
@@ -1,11 +1,31 @@
 ﻿#include <stdio.h>
 
-int binary_search(int* source_massiv, int count_elements)
+int binary_search(int* source_array, int count_elements, int item)
 {
+	int low, mid, high, guess;
+	low = 0;
+	mid = 0;
+	high = count_elements - 1;
+	guess = source_array[mid];
 
+	while (high > low)
+	{
+		mid = (high + low) / 2;
+		guess = source_array[mid];
+		if (guess == item)
+			return mid;
+		if (guess > item)
+			high = mid;
+		else
+			low = mid;
+	}
+	return 0;
 }
 
 int main()
 {
+	int arr[5] = { 1, 3, 5, 7, 9 };
+	int item = 7;
+	printf("Position \"%d\" in array = %d", item, binary_search(arr, 5, item) + 1);
 	return 0;
 }
